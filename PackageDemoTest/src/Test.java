ac6cc7c8fe5b3f39305586ba0f98dd311d423820
user <user@LAPTOP-L09TTONQ>
2019-12-26 20:32:19 +0800

    Initial commit message

---
+++ PackageDemoTest/src/Test.java
@@ -0,0 +1,14 @@
+
+import edu.nara.java.math.Calc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Calc calc=new Calc();
+		System.out.println(calc.add(10,11));
+		System.out.println(calc.getSumOfNnumbers(10));
+
+	}
+
+}
